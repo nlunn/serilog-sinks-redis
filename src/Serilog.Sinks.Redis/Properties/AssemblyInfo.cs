106b326b66ee1fb6936968f0b9defce8f960ccf0
Niels Christian Lunn <nilu@danskebank.dk>
2016-08-03 10:33:38 +0200

    Fixed nuspec

--- src/Serilog.Sinks.Redis/Properties/AssemblyInfo.cs
+++ src/Serilog.Sinks.Redis/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle( "Serilog.Sinks.Redis" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "zdx" )]
 [assembly: AssemblyCopyright( "Copyright © Serilog Contributors 2016" )]
+[assembly: AssemblyCompany( "nilu@danskebank.dk" )]
