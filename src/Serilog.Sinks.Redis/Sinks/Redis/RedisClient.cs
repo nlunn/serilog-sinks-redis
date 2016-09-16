e8de02ef58eef05cd46df9424cb408d8b514f935
Niels Christian Lunn <nilu@danskebank.dk>
2016-09-16 10:58:06 +0200

    Late instantiation of redis

--- src/Serilog.Sinks.Redis/Sinks/Redis/RedisClient.cs
+++ src/Serilog.Sinks.Redis/Sinks/Redis/RedisClient.cs
@@ -5,18 +5,38 @@ namespace Serilog.Sinks.Redis
 {
   public class RedisClient
   {
-    private readonly ISubscriber _subscriber;
+    private readonly RedisConfiguration _configuration;
+    private ISubscriber _subscriber;
+    private readonly object _subscriberLock = new object();
+
+    private ISubscriber Subscriber
+    {
+      get
+      {
+        if( _subscriber != null ) return _subscriber;
+
+        lock( _subscriberLock )
+        {
+          if( _subscriber == null )
+          {
+            var redis = ConnectionMultiplexer.Connect( _configuration.Host );
+            _subscriber = redis.GetSubscriber();
+          }
+        }
+
+        return _subscriber;
+      }
+    }
 
     public RedisClient( RedisConfiguration configuration )
     {
-      var redis = ConnectionMultiplexer.Connect( configuration.Host );
-      _subscriber = redis.GetSubscriber();
+      _configuration = configuration;
     }
 
     public void Publish( string message )
     {
       var key = Guid.NewGuid().ToString();
-      _subscriber.Publish( key, message );
+      Subscriber.Publish( key, message );
     }
   }
 }
